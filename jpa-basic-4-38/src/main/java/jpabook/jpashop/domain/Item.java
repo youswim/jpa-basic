bc786def1fd1c44f0fc24edfcca180d23a531884
youswim <youswim96@gmail.com>
2022-07-08 22:10:12 +0900

    4-end

---
+++ jpa-basic-4-38/src/main/java/jpabook/jpashop/domain/Item.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.domain;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ITEM_ID") //column 이름은 각 회사의 룰을 따르면 된다
+    private Long id;
+
+    private String name;
+    private int price;
+    private int StockQuantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return StockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        StockQuantity = stockQuantity;
+    }
+}
---
+++ jpa-basic-4-38/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,54 @@
+package jpabook.jpashop.domain;
+
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ORDERS") //order가 예약어로 잡혀서 못쓰는 DB도 있으므로 이렇게 사용
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
