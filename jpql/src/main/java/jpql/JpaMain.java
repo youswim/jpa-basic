bb61a834e09aa70a510c7359c023a350649af6b1
youswim <youswim96@gmail.com>
2022-07-21 12:27:40 +0900

    end

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -22,53 +22,18 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
         try {
-
-            Team team1 = new Team();
-            team1.setName("teamA");
-            em.persist(team1);
-
-            Team team2 = new Team();
-            team2.setName("teamB");
-            em.persist(team2);
-
-            Team team3 = new Team();
-            team3.setName("teamC");
-            em.persist(team3);
-
-            Member member1 = new Member();
-            member1.setUsername("member1");
-            member1.setTeam(team1);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("member2");
-            member2.setTeam(team1);
-            em.persist(member2);
-
-            Member member3 = new Member();
-            member3.setUsername("member3");
-            member3.setTeam(team2);
-            em.persist(member3);
-
-            Member member4 = new Member();
-            member4.setUsername("member1");
-            em.persist(member4);
-
-            em.flush();
-            em.clear();
-
-            String query = "select t from Team t join t.members";
-            List<Team> Teams = em.createQuery(query, Team.class).getResultList();
-
-            for (Team team : Teams) {
-                System.out.println("team.getName() = " + team.getName());
-                for (Member member : team.getMembers()) {
-                    System.out.println(member);
-                }
-            }
-
-
-            tx.commit();
+Member member1 = new Member();
+member1.setUsername("member1");
+member1.setAge(10);
+em.persist(member1);
+int i = em.createQuery("update Member m set m.age = 20")
+        .executeUpdate();
+Member member = em.find(Member.class, member1.getId());
+System.out.println("member.getAge() = " + member.getAge());
+em.clear();
+Member findMember = em.find(Member.class, member1.getId());
+System.out.println("findMember.getAge() = " + findMember.getAge());
+tx.commit();
 
         } catch (Exception e) {
             tx.rollback();
